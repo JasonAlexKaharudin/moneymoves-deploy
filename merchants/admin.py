c00235a985e93e0b9b16778068ee10999a7dc46b
Jason A Kaharudin <jasonakaharudin@Jasons-Mac-mini.local>
2021-08-15 22:38:11 +0800

    removed Product

--- merchants/admin.py
+++ merchants/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Merchant, Product
+from .models import Merchant
 # Register your models here.
 
 admin.site.register(Merchant)
-admin.site.register(Product)
