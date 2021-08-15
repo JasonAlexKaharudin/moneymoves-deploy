98563b744a279e231076d63d068e1084caaef270
Jason A Kaharudin <jasonakaharudin@Jasons-Mac-mini.local>
2021-08-15 22:08:08 +0800

    fixed the brands page

--- merchants/models.py
+++ merchants/models.py
@@ -11,15 +11,3 @@ class Merchant(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Product(models.Model):
-    product_ID = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    merchant = models.ForeignKey(Merchant, on_delete=CASCADE)
-
-    class Meta:
-        verbose_name_plural = "Products"
-        
-    def __str__(self):
-        return self.name
