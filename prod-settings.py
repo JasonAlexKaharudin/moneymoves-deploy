87d75a8ca36a6150a5262b6854b465e049ff3f23
Jason A Kaharudin <jasonakaharudin@Jasons-Mac-mini.local>
2021-08-30 14:29:54 +0800

    added usersplit analytics

--- prod-settings.py
+++ prod-settings.py
@@ -164,3 +164,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'getmoneymoves@gmail.com'
 EMAIL_HOST_PASSWORD = 'aqqjoxhsylbufpcr'
+ALLOWED_HOSTS = os.getenv("EMAIL_HOST_PASSWORD", '')
