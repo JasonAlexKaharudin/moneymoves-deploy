2adb55159b70dacdc392e3212560aa5845202422
Jason Alexander Kaharudin <jasonakaharudin@gmail.com>
2021-10-08 05:28:31 -0700

    enable email sending for new registers

--- users/views.py
+++ users/views.py
@@ -96,7 +96,7 @@ def register(request, code):
             plain_message = strip_tags(html_message)
             from_email = settings.EMAIL_HOST_USER
             to = user.email
-            # mail.send_mail(subject, plain_message, from_email,[to], html_message = html_message)
+            mail.send_mail(subject, plain_message, from_email,[to], html_message = html_message)
 
             login(request, new_user)
             return redirect('brands')
