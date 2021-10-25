9c1e29e78e196b92ea34cc3a76caa45cd7dc0cc9
Jason Alexander Kaharudin <jasonakaharudin@gmail.com>
2021-10-24 22:39:00 -0700

    temporarily remove signal for postwebhook

--- merchants/signals.py
+++ merchants/signals.py
@@ -4,26 +4,26 @@ from .models import webhookOrders
 import api.models
 import referrals.models
 
-@receiver(post_save, sender = webhookOrders)
-def post_save_webhook(sender, instance, created, *args, **kwargs):
-    if created:
-        #match this object with an orderRef object
-        orderRef_obj = api.models.orderRef.objects.filter(merchant_name = instance.merchant)
-        orderRef_obj = orderRef_obj.filter(orderID = instance.order_id)[0]
-        orderRef_obj.webhook_obj = instance
-        orderRef_obj.save()
+# @receiver(post_save, sender = webhookOrders)
+# def post_save_webhook(sender, instance, created, *args, **kwargs):
+#     if created:
+#         #match this object with an orderRef object
+#         orderRef_obj = api.models.orderRef.objects.filter(merchant_name = instance.merchant)
+#         orderRef_obj = orderRef_obj.filter(orderID = instance.order_id)[0]
+#         orderRef_obj.webhook_obj = instance
+#         orderRef_obj.save()
 
-        #create a new referral object
-        ref_obj = referrals.models.Referral.objects.create(
-            referer_username = orderRef_obj.referrer,
-            merchant = instance.merchant,
-            sessionID = orderRef_obj.sessionID,
-            orderID = instance.order_id,
-            totalAmt = instance.total_price,
-            products = instance.products,
-            referee_email = instance.customer_email,
-            orderRef_obj = orderRef_obj
-        )
+#         #create a new referral object
+#         ref_obj = referrals.models.Referral.objects.create(
+#             referer_username = orderRef_obj.referrer,
+#             merchant = instance.merchant,
+#             sessionID = orderRef_obj.sessionID,
+#             orderID = instance.order_id,
+#             totalAmt = instance.total_price,
+#             products = instance.products,
+#             referee_email = instance.customer_email,
+#             orderRef_obj = orderRef_obj
+#         )
 
-        ref_obj.save()
-        instance.save()
+#         ref_obj.save()
+#         instance.save()
