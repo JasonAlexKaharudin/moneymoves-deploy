cd75ceba57839b4443d60fc1827aa4c61fe433dd
Jason Alexander Kaharudin <jasonakaharudin@gmail.com>
2021-11-06 16:24:47 -0700

    bug fixed

--- api/admin.py
+++ api/admin.py
@@ -2,11 +2,11 @@ from webapp.adminMixins import ExportCSVMixin
 from django.contrib import admin
 from .models import orderRef, invalidOrder, trackWidget, involveAsia_PostbackURL, Order_Controller
 
-@admin.register(Order_Controller)
-class Order_ControllerAdmin(admin.ModelAdmin, ExportCSVMixin):
-    list_display = ("orderRef_obj", "webhook", "matched")
-    list_filter = ("matched")
-    actions = ["export_as_csv"]
+# @admin.register(Order_Controller)
+# class Order_ControllerAdmin(admin.ModelAdmin, ExportCSVMixin):
+#     list_display = ("orderRef_obj", "webhook", "matched")
+#     list_filter = ("matched")
+#     actions = ["export_as_csv"]
 
 @admin.register(trackWidget)
 class widgetAdmin(admin.ModelAdmin, ExportCSVMixin):
